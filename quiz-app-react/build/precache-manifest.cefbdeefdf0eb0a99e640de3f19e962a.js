9f771ea5520544e31b2eea6d6e0a4b21c8cd3e77
Ashwin Balaji R <ashwinbalaji.r@cognizant.com>
2019-07-31 18:37:47 +0530

    quip app react commit

---
+++ quiz-app-react/build/precache-manifest.cefbdeefdf0eb0a99e640de3f19e962a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d343a0f29205f158592ce12d65ad7ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2eb5c9ada630d500e8a",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "578bfaca91cc9b5e6fde",
+    "url": "/static/css/main.72fff3ff.chunk.css"
+  },
+  {
+    "revision": "d2eb5c9ada630d500e8a",
+    "url": "/static/js/2.d6429e0f.chunk.js"
+  },
+  {
+    "revision": "578bfaca91cc9b5e6fde",
+    "url": "/static/js/main.3b1d5506.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
